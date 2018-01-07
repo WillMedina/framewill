9041dd45a2507cbf51d6005eac573c29282d3bf2
Will Medina <will.med.s16@gmail.com>
2018-01-07 03:05:34 -0500

    Agregadas funcionalidades extra para el manejo de vista, saneamiento y el buffer para comprimir salida.

---
+++ model/utils.php
@@ -0,0 +1,63 @@
+<?php
+
+class utils {
+
+    static function output_html($stream, $array_cambios = null) {
+        if (!is_null($array_cambios)) {
+            $archivo = file_get_contents($stream);
+            $indices = array_keys($array_cambios);
+            $valores = array_values($array_cambios);
+            $stream = str_replace($indices, $valores, $archivo);
+            return $stream;
+        } else {
+            return $stream;
+        }
+    }
+
+    static function input_sanitize($string) {
+        $array_danger = array("'", '"', '\u2019', '%', '&#8217;');
+        $string = str_replace($array_danger, "", $string);
+        $string = filter_var($string, FILTER_SANITIZE_STRING);
+        return $string;
+        //return mysqli_real_escape_string($string); // ok no
+        //pendiente buscar una forma de sanitizar esto
+    }
+
+    static function sanitize_output($buffer) {
+
+        $search = array(
+            '/\>[^\S ]+/s', // strip whitespaces after tags, except space
+            '/[^\S ]+\</s', // strip whitespaces before tags, except space
+            '/(\s)+/s', // shorten multiple whitespace sequences
+            '/<!--(.|\s)*?-->/' // Remove HTML comments
+        );
+
+        $replace = array(
+            '>',
+            '<',
+            '\\1',
+            ''
+        );
+
+        $buffer = preg_replace($search, $replace, $buffer);
+
+        return $buffer;
+    }
+
+    static function time_UnixToMySQL($unixtime) {
+        $mysql_time = date('Y-m-d H:i:s', $unixtime);
+        return $mysql_time;
+    }
+
+    static function time_MySQLToUnix($mysqltime) {
+        $timestamp = strtotime($mysqltime);
+        return $timestamp;
+    }
+
+    static function formatTime($mysqltime, $format = "d-m-Y g:i A") {
+        $time = strtotime($mysqltime);
+        $myFormatForView = date($format, $time);
+        return $myFormatForView;
+    }
+
+}
